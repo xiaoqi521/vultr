0b939feed0a6e0502e6634299e9f019aab46a07b
seven <zhuhao@ouchteam.com>
2018-03-30 11:32:45 +0800

    fix bug

--- vultr1/vultr/urls.py
+++ vultr1/vultr/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     url(r'^', include('server.urls')),
-    url(r'^admin/', admin.site.urls),
 ]
+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
